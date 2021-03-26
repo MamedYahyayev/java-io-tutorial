e2493a5e836f090f6a1a081bda6f28880898a9c8
Mamed Yahyayev <yahyayev2001@gmail.com>
2021-03-26 20:00:40 +0400

    utility for our project

---
+++ src/az/maqa/java/util/Utility.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package az.maqa.java.util;
+
+/**
+ *
+ * @author mamedyahyayev
+ */
+public final class Utility {
+
+    private Utility() {
+
+    }
+
+    public static void printByteArray(byte[] b) {
+        for (byte value : b) {
+            System.out.print(value + " ");
+        }
+    }
+}
